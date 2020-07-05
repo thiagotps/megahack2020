db84dc1fd90d2abff5744505e5fa6bb275d8d5d0
Thiago T. P. Silva <thiagoteodoro501@gmail.com>
2020-07-05 13:29:00 -0300

    Authentication Model

--- api_embarcacao/main.py
+++ api_embarcacao/main.py
@@ -1,11 +1,39 @@
+from datetime import datetime, timedelta
 from typing import Optional
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt import PyJWTError
+from passlib.context import CryptContext
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+# This should be stored in a file and read, not directly in the code
+SECRET_KEY = "ea6f33d15fb45c513c1a2762b9919672789efb206e8692cea1613e891cacd3ed"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+fake_users_db = {
+    "johndoe@example.com": {
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+    }
+}
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 class User(BaseModel):
     email: str
-    senha: str
+    full_name: str
+    hashed_password: str
+
+# Object responsible to hash our passwords
+PWD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
+OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl="/token")
 
 app = FastAPI()
 
@@ -17,19 +45,64 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-async def readRoot():
-    return {"Hello": "World"}
+def get_user(db, email: str):
+    if email in db:
+        user_dict = db[email]
+        return User(**user_dict)
 
+def authenticate_user(db, email: str, password: str):
+    user = get_user(db, email)
+    if user:
+        if not PWD_CONTEXT.verify(password, user.hashed_password):
+            return None
 
-@app.post("/login")
-async def login(user: User):
-	email = user.email
-	senha = user.senha
-	return {"login_status": True}
+    return user
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta]):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(OAUTH2_SCHEME)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except PyJWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, email=username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.email}, expires_delta=access_token_expires)
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
 
 @app.get("/avaliacoes")
-async def getResultadoAvaliacoes():
+async def get_resultado_avaliacoes():
     return {
     	"ruim": 30,
     	"medio": 30,
@@ -38,7 +111,7 @@ async def getResultadoAvaliacoes():
     }
 
 @app.get("/cursos")
-async def getCursos():
+async def get_cursos():
     return [
     	{
 			"nome": "curso 1",
